da456c09dfc5041c3e7be61ecf4313b341ec822e
Zach <wakuwakuzakku@gmail.com>
2023-11-21 17:35:45 +0900

    Fix date not showing up after saved bug

--- components/SavePicScreen.js
+++ components/SavePicScreen.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import { Button } from 'react-native-paper';
 import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
 
 const SavePicScreen = ({ navigation, route }) => {
   const { userName, dateTimeString, photoURI } = route.params;
   const [mediaLibraryPermission, setMediaLibraryPermission] = useState(null);
+  const [capturedImageURI, setCapturedImageURI] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -17,16 +19,32 @@ const SavePicScreen = ({ navigation, route }) => {
   const savePhotoToGallery = async () => {
     try {
       if (mediaLibraryPermission) {
-        const asset = await MediaLibrary.createAssetAsync(photoURI);
-        console.log('Photo saved to gallery:', asset);
+        // if persmission is granted, capture the view and save the image
+        const uri = await captureRef(capturedViewRef, {
+          format: 'jpg',
+          quality: 0.8,
+        });
+
+        // Save the image to the media library
+        const asset = await MediaLibrary.createAssetAsync(uri);
+        console.log('Photo saved to galler:', asset);
+
+        // Extract the creation time of the asset
         const creationTime = asset.creationTime;
+
+        // Convert the creation time to a localized date and time string
         const date = new Date(creationTime);
-        const dateString = date.toLocaleString(); // Convert to a localized date and time string
+        const dateString = date.toLocaleString();
+
+        // Log the creation time as a formatted date string
         console.log('Creation Time:', dateString);
+
       } else {
+        // Log an error message if media library permission is not granted
         console.error('Permission to access media library not granted.');
       }
     } catch (error) {
+      // Log an error message if there's an issue saving the photo to the gallery
       console.error('Error saving photo to gallery:', error);
     }
   };
@@ -34,19 +52,27 @@ const SavePicScreen = ({ navigation, route }) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 26, marginBottom: 10 }}>{userName}</Text>
-      <View style={styles.imageContainer}>
+      <View style={styles.imageContainer} ref={(view) => (capturedViewRef = view)}>
         {photoURI && (
-          <ImageBackground
+          <Image
             source={{ uri: photoURI }}
             style={styles.image}
             resizeMode="cover"
-          >
+          />
+        )}
+        {capturedImageURI && (
+          <Image
+            source={{ uri: capturedImageURI }}
+            style={styles.image}
+            resizeMode="cover"
+            />
+        )}
             <View style={styles.overlay}>
               <Text style={styles.overlayText}>{dateTimeString}</Text>
             </View>
-          </ImageBackground>
-        )}
-      </View>
+          </View>
+        
+      
 
       <Button
         style={{ marginTop: 16 }}
